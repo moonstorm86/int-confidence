3f9102b23182d5bcb58c7dc0c6e99522a2ec7017
moonstorm86 <cswan2@id.iit.edu>
2019-03-13 18:58:10 -0500

    Update reddit_sentimentScrape.py

--- reddit_sentimentScrape.py
+++ reddit_sentimentScrape.py
@@ -3,11 +3,11 @@ import datetime as dt
 import pandas as pd
 from pandas import DataFrame
 
-reddit = praw.Reddit(client_id='mZMu1hzVkJclGg',
-                     client_secret='AnQMjtuOAKOBSSJafkDCOUK-kRE',
-                     password='xasdr5k7',
-                     user_agent='hot_taco',
-                     username='Moonstorm0725')
+reddit = praw.Reddit(client_id='=======',
+                     client_secret='=======',
+                     password='=======',
+                     user_agent='=======',
+                     username='=======')
 
 submission = reddit.submission(id='7v8ob2')
 
@@ -17,4 +17,4 @@ df_rows = [[comment.parent, comment.id, comment.score, comment.created, comment.
 
 df = pd.DataFrame(df_rows, columns=['Parent ID', 'Comment ID', 'Score', 'Created', 'Body','Ups','Downs'])
 
-df.to_csv('google_home.csv',encoding='utf-8')
+df.to_csv('data_scrape.csv',encoding='utf-8')
